8ed2e62edc7d62dd472d13262508fe4938a8f726
Kuldip Sirpali <151803791+Kuldip-Sirpali@users.noreply.github.com>
2025-07-19 13:39:53 +0545

    remove one video iframe

--- JS/data.js
+++ JS/data.js
@@ -65,12 +65,7 @@ const userData = [
   
 ];
 
-const users = {
-  userName: "Will smith",
-  followerCount: 45,
-  post: `<iframe width="560" height="315" src="https://www.youtube.com/embed/lOKASgtr6kU?si=KVaRtyQL0GkBhjMI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>",
-};
-userData.push(users);
+
 
 // const datas=structuredClone(userData);
 // console.log(datas);
